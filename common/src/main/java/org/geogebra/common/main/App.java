46561582f40596d5b6016b571f77a883fc400f8f
Zbynek Konecny <zbynek@geogebra.at>
2020-08-13 19:36:42 +0200

    WLY-48 Remove feature flag, serialization fix

--- common/src/main/java/org/geogebra/common/main/App.java
+++ common/src/main/java/org/geogebra/common/main/App.java
@@ -3878,9 +3878,6 @@ public abstract class App implements UpdateSelection, AppInterface, EuclidianHos
 		case ADJUST_WIDGETS:
 			return false;
 
-		case SURFACE_2D:
-			return prerelease;
-
 		case SYMBOLIC_AV:
 			return true;
 
